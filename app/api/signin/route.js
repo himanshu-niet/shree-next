fe2aa2578692926c81701ba06e249ed789db8b52
Himanshu <108611598+himanshu-niet@users.noreply.github.com>
2023-12-16 16:08:56 +0000

    jj

--- app/api/signin/route.js
+++ app/api/signin/route.js
@@ -1,41 +1,41 @@
 
-import prisma from '@/lib/prisma';
-import jwt from '@/utils/jwt';
-import { NextResponse } from 'next/server';
-const bcrypt = require('bcryptjs');
+// import prisma from '@/lib/prisma';
+// import jwt from '@/utils/jwt';
+// import { NextResponse } from 'next/server';
+// const bcrypt = require('bcryptjs');
 
-export async function POST(request) {
+// export async function POST(request) {
 
-    try {
+//     try {
         
-        let {email,password} = await request.json();
-        // let hashPassword=bcrypt.hashSync(password, 8);
+//         let {email,password} = await request.json();
+//         // let hashPassword=bcrypt.hashSync(password, 8);
 
-        const user = await prisma.user.findUnique({
-            where: {
-              email: email,
-            },
-          })
-          console.log("user")
+//         const user = await prisma.user.findUnique({
+//             where: {
+//               email: email,
+//             },
+//           })
+//           console.log("user")
 
-return NextResponse.json({"query":"query"})
-        // user.accessToken = await jwt.signAccessToken(user);
-        // return NextResponse.json({
-        //     success: true,
-        //     data: user
-        // }, {
-        //     status: 201,
-        // });
-    } catch (error) {
-        return NextResponse.json({
-            success: false,
-            error: error.message
-        }, {
-            status: 501,
-        });
-    } finally {
-        await prisma.$disconnect();
-    }
+// return NextResponse.json({"query":"query"})
+//         // user.accessToken = await jwt.signAccessToken(user);
+//         // return NextResponse.json({
+//         //     success: true,
+//         //     data: user
+//         // }, {
+//         //     status: 201,
+//         // });
+//     } catch (error) {
+//         return NextResponse.json({
+//             success: false,
+//             error: error.message
+//         }, {
+//             status: 501,
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
 
 
-}
+// }
--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -1,46 +1,46 @@
 
-import prisma from '@/lib/prisma';
-import jwt from '@/utils/jwt';
-import { NextResponse } from 'next/server';
-const bcrypt = require('bcryptjs');
+// import prisma from '@/lib/prisma';
+// import jwt from '@/utils/jwt';
+// import { NextResponse } from 'next/server';
+// const bcrypt = require('bcryptjs');
 
-export async function POST(request) {
+// export async function POST(request) {
 
-    try {
-        let {name, email,password,phone} = await request.json();
+//     try {
+//         let {name, email,password,phone} = await request.json();
 
-        if (!name, !email,!password,!phone) {
-            return NextResponse.json({
-                success: false,
-                error:"Bad Request"
-            }, {
-                status: 400,
-            });
-        }
+//         if (!name, !email,!password,!phone) {
+//             return NextResponse.json({
+//                 success: false,
+//                 error:"Bad Request"
+//             }, {
+//                 status: 400,
+//             });
+//         }
 
-        password=bcrypt.hashSync(password, 8);
+//         password=bcrypt.hashSync(password, 8);
 
-        const user = await prisma.user.create({
-            data: {name,email,password,phone},
-        });
+//         const user = await prisma.user.create({
+//             data: {name,email,password,phone},
+//         });
 
-        user.accessToken = await jwt.signAccessToken(user);
-        return NextResponse.json({
-            success: true,
-            data: user
-        }, {
-            status: 201,
-        });
-    } catch (error) {
-        return NextResponse.json({
-            success: false,
-            error: error.message
-        }, {
-            status: 501,
-        });
-    } finally {
-        await prisma.$disconnect();
-    }
+//         user.accessToken = await jwt.signAccessToken(user);
+//         return NextResponse.json({
+//             success: true,
+//             data: user
+//         }, {
+//             status: 201,
+//         });
+//     } catch (error) {
+//         return NextResponse.json({
+//             success: false,
+//             error: error.message
+//         }, {
+//             status: 501,
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
 
 
-}
+// }
