d4f92b8886101d34617e13c92fb359a37008dc58
Himanshu <16himanshu16yadav@gmail.com>
2023-12-24 13:53:09 +0000

    d

--- app/api/admin/product/route.js
+++ app/api/admin/product/route.js
@@ -4,56 +4,69 @@ import { NextResponse } from "next/server";
 export async function POST(request){
 try {
 
-    const {
-        title,
-        desc,
-        price,
-        discount,
-        category,
-        subCategory,
-        color,
-        stock,
-        images
-    }=await request.json();
+    // const res=await request.json()
+    // console.log(res)
 
-    if (!title,
-        !desc,
-        !price,
-        !discount,
-        !category,
-        !subCategory,
-        !color,
-        !stock,
-        !images) {
-        return NextResponse.json({
-            success: false,
-            error:"Bad Request"
-        }, {
-            status: 400,
-        });
-    }
-    const product= await prisma.product.create({
-        data:{
-            title,
-            desc,
-            price,
-            discount,
-            category,
-            subCategory,
-            color,
-            stock,
-            images
-        }
-    })
+
+    // const {
+    //     title,
+    //     desc,
+    //     price,
+    //     discount,
+    //     category,
+    //     subCategory,
+    //     color,
+    //     stock,
+    //     images
+    // }=await request.json();
+
+    // if (!title,
+    //     !desc,
+    //     !price,
+    //     !discount,
+    //     !category,
+    //     !subCategory,
+    //     !color,
+    //     !stock,
+    //     !images) {
+    //     return NextResponse.json({
+    //         success: false,
+    //         error:"Bad Request"
+    //     }, {
+    //         status: 400,
+    //     });
+    // }
+
+
+
+    // const product= await prisma.product.create({
+    //     data:{
+    //         title,
+    //         desc,
+    //         price,
+    //         discount,
+    //         category,
+    //         subCategory,
+    //         color,
+    //         stock,
+    //         images:"test.png"
+    //     }
+    // })
+
+    // return NextResponse.json({
+    //     success: true,
+    //     data: product
+    // }, {
+    //     status: 201,
+    // });
 
     return NextResponse.json({
-        success: true,
-        data: product
-    }, {
-        status: 201,
-    });
+success:false,
+    },{status:599})
   
 } catch (error) {
+    console.log(error)
+    
 return NextResponse.json({
     success: false,
     error: error.message
